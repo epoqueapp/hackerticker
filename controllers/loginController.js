06e33fd28f8416e2f9bce799389c82f5a1c4dc67
Maximilian Alexander <max@zinkpulse.com>
2015-05-13 15:32:27 -0700

    fixing pass

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -8,7 +8,7 @@ var User = require('../data/User');
 var express = require('express');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
-var callbackUrl = process.env.NODE_ENV == "prod" ? "http://www.hackerticker.comauth/twitter/callback" : "http://127.0.0.1:3000/auth/twitter/callback";
+var callbackUrl = process.env.NODE_ENV == "prod" ? "http://www.hackerticker.com/auth/twitter/callback" : "http://127.0.0.1:3000/auth/twitter/callback";
 var TWITTER_CONSUMER_KEY = process.env.NODE_ENV == "prod" ? "Q3URyMneq4swr47v5Xs5pQU8e" : "SiRnNpKyTMu5g2d0064nN9jZ3";
 var TWITTER_CONSUMER_SECRET = process.env.NODE_ENV == "prod" ? "P6JAgcsRCETyQqeZSb8cIxXIumz5A2pFLLmuKv4C9ajqmmC8DG" : "FRk0jtWOcBn7T44xboeFFDcvhobOrdg9IwhLfV6yEz4VE3l8Fl";
 
